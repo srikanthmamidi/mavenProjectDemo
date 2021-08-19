da254097887f69510ae5d7f4387d153a19059364
SRIKANTH MAMIDIPALLI <89125621+srikanthmamidi@users.noreply.github.com>
2021-08-19 21:21:08 +0530

    Update Maventest.java

--- src/test/java/com/srikanth/mavenDemo/Maventest.java
+++ src/test/java/com/srikanth/mavenDemo/Maventest.java
@@ -9,11 +9,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Maventest {
 @Test
 	public void openBro() throws InterruptedException {
-
+		System.out.println("browser opened");
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver =  new ChromeDriver();
 		driver.manage().window().maximize();
+		System.out.println("browser maximized opebed google.com");
 		driver.get("https://www.google.com");
+		System.out.println("url entered");
 		Thread.sleep(3000);
 
 	}
